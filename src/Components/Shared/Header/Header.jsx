f250f5294d15f6cca2ec116f631c67effca71f5c
Anik Barua <anikbb04@gmail.com>
2023-07-11 06:09:33 +0600

    change banner svg and add href in header

--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -8,17 +8,21 @@ const Header = () => {
             <nav>
                 <div className="flex flex-wrap justify-between py-2">
                     <div className="flex ">
-                       <a href=""> <img className='w-[72px] h-16 ' src={logo} alt="" /></a>
-                       <p className='text-2xl font-semibold pt-5' >Bhojan Mitra</p>
+                        <a href=""> <img className='w-[72px] h-16 ' src={logo} alt="" /></a>
+                        <p className='text-2xl font-semibold pt-5' >Bhojan Mitra</p>
                     </div>
                     <div className="">
-                       
+
                     </div>
                     <div className="">
-                    <ul className='flex flex-wrap mt-3'>
-                            
-                            <li className='mx-3'><a href="">Features</a> </li>
-                            <li className='mx-3'><a href="">FAQ</a> </li>
+                        <ul className='flex flex-wrap mt-3'>
+
+                            <li className='mx-3'>
+                                <a href="#feature">Features</a>
+                            </li>
+                            <li className='mx-3'>
+                                <a href="#faq">FAQ</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
